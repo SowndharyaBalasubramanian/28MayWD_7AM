ca90fe7bd95270928cc3db176536cfa234ff1543
Sowndharya <Sowndharya0105@gmail.com>
2025-06-20 08:00:54 +0530

    Ternary operator

---
+++ src/com/operators/Ternary.java
@@ -0,0 +1,24 @@
+package com.operators;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int age = 7;
+//        if(age >=18){
+//            System.out.println("major");
+//        }else {
+//            System.out.println("minor");
+//        }
+
+        //condition ? true : false
+
+        String result = (age>=18) ? "major": "minor";
+        System.out.println(result);
+
+        //even or odd
+        int num= 50;
+        String answer = (num%2==0) ? "Even" : "odd";
+        System.out.println(answer);
+    }
+}
+
+//find the greatest number using ternary operator with 2 int and 3 int
