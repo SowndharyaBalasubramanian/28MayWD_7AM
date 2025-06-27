4ad56cefda5955d9555249a0aabf62ccdbee4bd1
Sowndharya <Sowndharya0105@gmail.com>
2025-06-27 07:54:02 +0530

    polymorphism

---
+++ src/com/polymorphism/Method_Overriding.java
@@ -0,0 +1,32 @@
+package com.polymorphism;
+
+public class Method_Overriding {
+    public static void main(String[] args){
+//        Parent p = new Parent();
+//        p.education();
+
+        Child c = new Child();
+        c.show();
+        c.education();
+
+    }
+}
+
+class Parent{
+
+    public void education(){
+        System.out.println("to become Doctor");
+    }
+}
+
+class Child extends Parent{
+    @Override
+
+    public void education() {
+        System.out.println("to become trainer");
+    }
+
+    public void show(){
+        System.out.println("I am from child");
+    }
+}
