07c36d1a3748459c5d14d76baaaf8a225e55a1da
Sowndharya <Sowndharya0105@gmail.com>
2025-07-16 08:06:13 +0530

    multithreading

---
+++ src/com/multithreadingg/Thread_Methods.java
@@ -0,0 +1,59 @@
+package com.multithreadingg;
+
+import java.io.PrintStream;
+
+public class Thread_Methods {
+    public static void main (String[] args){
+        ThreadSample t1 = new ThreadSample();
+        t1.setName("Thread A");
+        t1.setPriority(10);
+        t1.start();
+
+        ThreadSample t2 = new ThreadSample();
+        t2.setName("Thread B");
+        t2.setPriority(1);
+        t2.start();
+
+        ThreadSample t3 = new ThreadSample();
+        t3.setName("Thread C");
+        t3.setPriority(5);
+        t3.start();
+
+        try{
+            Thread.sleep(3500);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("main thread is interrupting");
+        t1.interrupt();
+
+
+        try{
+//            t1.run();
+//        } catch (InterruptedException e) {
+//            System.out.println(e);
+//        }
+
+    }
+}
+
+class ThreadSample extends Thread {
+    @Override
+    public void run() {
+        try {
+            print();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("run method");
+    }
+         public void print() throws InterruptedException{
+            for(int i = 1; i <5; i++){
+                System.out.println("Count : " + i + " " + getPriority() );
+                Thread.sleep(2000);
+
+            }
+
+    }
+}
